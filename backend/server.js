940db7721e9d525391e8e6d1b003307c71fdf4a5
KabootarGod <aryansinha1908@gmail.com>
2026-05-26 15:16:19 +0530

    Initialised backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const Port = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log(`The server is running on port: ${Port}`);
+})
